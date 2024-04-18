c5c0d54d5f00329650179ba7e9cf6aaffd1a8878
adityaraj14-12 <adityapersonalac@gmail.com>
2024-04-19 00:11:29 +0530

    splash and login is ready

---
+++ src/Electricity/Billing/System/ElectricityBillingSystem.java
@@ -0,0 +1,10 @@
+package Electricity.Billing.System;
+
+public class ElectricityBillingSystem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Electricity/Billing/System/Signup.java
@@ -0,0 +1,15 @@
+package Electricity.Billing.System;
+
+import javax.swing.*;
+public class Signup extends JFrame{
+	
+	Signup(){
+		setSize(700,400);
+		setLocation(450,150);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new Signup();
+	}
+}
